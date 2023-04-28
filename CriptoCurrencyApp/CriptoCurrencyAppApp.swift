93662465aa0b5572c3289af0bb8e447806c9fd37
Balakrishnan23962 <balakrishnan5523962@gmail.com>
2023-04-28 15:48:30 +0530

    Initial Commit

---
+++ CriptoCurrencyApp/CriptoCurrencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CriptoCurrencyAppApp.swift
+//  CriptoCurrencyApp
+//
+//  Created by Gasc_Internship1 on 28/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CriptoCurrencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
